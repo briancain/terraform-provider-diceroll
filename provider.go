11d4b739e4176c10c19f98a832a1816e9ab0437d
Brian Cain <bcain@hashicorp.com>
2020-08-21 09:11:23 -0700

    Add basic files for provider

---
+++ provider.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+// Provider returns a Terraform provider schema.
+func Provider() *schema.Provider {
+}
---
+++ resource_diceroll.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+)
+
+func resourceDiceRoll() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceDiceRollCreate,
+		ReadContext:   resourceDiceRollRead,
+		UpdateContext: resourceDiceRollUpdate,
+		DeleteContext: resourceDiceRollDelete,
+	}
+}
+
+func resourceDiceRollCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	return nil
+}
+
+func resourceDiceRollRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	return nil
+}
+
+func resourceDiceRollUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	return nil
+}
+
+func resourceDiceRollDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	return nil
+}
