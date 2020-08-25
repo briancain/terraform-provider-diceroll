b4399a307a01db4215166838565330410201613a
Brian Cain <bcain@hashicorp.com>
2020-08-25 15:41:50 -0700

    Move provider and resource files into internal folder

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/plugin"
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+
+	"github.com/briancain/terraform-provider-diceroll/internal/provider"
 )
 
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: func() terraform.ResourceProvider {
-			return Provider()
+			return diceroll.Provider()
 		},
 	})
 }
