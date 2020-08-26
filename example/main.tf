3e1231a252f7017edd6eb1a1dbdc3cbae0ac8c06
Brian Cain <bcain@hashicorp.com>
2020-08-26 15:36:56 -0700

    terraform fmt main.tf file

--- example/main.tf
+++ example/main.tf
@@ -1,14 +1,18 @@
 provider "diceroll" {
-  source = "briancain/diceroll"
+  source  = "briancain/diceroll"
   version = "0.0.5"
 }
 
-resource "dice" "Yahtzee" {
+resource "dice" "yahtzee" {
   dice {
     die {
-      id = 1
+      id    = 1
       sides = 6
     }
     quantity = 6
   }
+}
+
+output "dice_roll" {
+  value = dice.yahtzee
 }
